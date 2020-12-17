2c50784397a90b8184d98c6b85b3af9c897e842c
jakub0227 <68559474+jakub0227@users.noreply.github.com>
2020-12-17 15:18:21 +0100

    Start new template app, basic layout with Browser Router, Material-UI, Emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {FC} from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {StylesProvider, Typography} from '@material-ui/core'
 import {Layout} from './hoc/Layout/Layout'
 import {Theme} from './hoc/Theme/Theme'
@@ -6,16 +7,25 @@ import {Theme} from './hoc/Theme/Theme'
 export const App: FC = () => {
 	
 	return (
-		<div>
+		<BrowserRouter>
 			<StylesProvider injectFirst>
 				<Theme>
 					<Layout>
-						<Typography variant='h1'>
-							Hello World!
-						</Typography>
+						<Switch>
+							<Route exact path='/'>
+								<Typography variant='h1'>
+									Hello from Main Page!
+								</Typography>
+							</Route>
+							<Route>
+								<Typography variant='h1'>
+									404 Not Found
+								</Typography>
+							</Route>
+						</Switch>
 					</Layout>
 				</Theme>
 			</StylesProvider>
-		</div>
+		</BrowserRouter>
 	)
 }
